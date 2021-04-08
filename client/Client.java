5ceb38be05a794493908dc6fe36c8d4c4d43cbb3
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-04-08 17:43:41 +0100

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -2,10 +2,12 @@ package client;
 
 import shared.OSserver;
 
+import java.io.FileNotFoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.List;
 import java.util.Scanner;
 
 public class Client {
@@ -14,9 +16,9 @@ public class Client {
 
     public void display_menu() {
         System.out.println("-----------");
-        System.out.println("   Menu   ");
+        System.out.println("    Menu   ");
         System.out.println("-----------");
-        System.out.println ( "1) Option 1\n2) Option 2\n" );
+        System.out.println ( "1) Display Products \n2) Option 2\n" );
         System.out.print ( "Selection: " );
     }
 
@@ -30,7 +32,7 @@ public class Client {
         System.out.println("Enter CustomerID: ");
         String username = in.nextLine();
         System.out.println("Enter Password: ");
-        String password = in .nextLine();
+        String password = in.nextLine();
 
         try {
             String serv_response = server.userlogin(username,password); //calling the remote method using the obtained object we got from registry (the server).
@@ -42,12 +44,23 @@ public class Client {
                 display_menu();
 
                 switch (in.nextInt()) {
-                    case 1 -> System.out.println("You picked option 1");
+                    case 1 -> {
+                       List<String[]> lines = server.getProducts(); //gets list of products & information from server
+                       
+                       for ( String[] line : lines){
+                           String name = line[0];
+                           int quantity = Integer.parseInt(line[1]);
+                           int restockd = Integer.parseInt(line[2]);
+                           int restockq = Integer.parseInt(line[3]);
+                           System.out.println("Name: " + name + ", Quantity: " + quantity + ", Restock Date: " + restockd + ", Restock Quantity: " + restockq);
+                        }
+                       
+                    }
                     case 2 -> System.out.println("You picked option 2");
                     default -> System.err.println("Unrecognized option");
                 }
             }
-        } catch (RemoteException e) {
+        } catch (RemoteException | FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Error");
         }
