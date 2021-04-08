8fccad69bfa5f5ed734ac6d95ea6cbaecc7c1187
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-04-08 17:44:02 +0100

    Update OSserver.java

--- shared/OSserver.java
+++ shared/OSserver.java
@@ -1,10 +1,13 @@
 package shared;
 
+import java.io.FileNotFoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 //Remote interface - this provides the description of all methods of a particular remote object.
 //The client communicates with this remote interface.
 public interface OSserver extends Remote {
     String userlogin(String username, String password) throws RemoteException;
+    List<String[]> getProducts() throws RemoteException, FileNotFoundException;
 }
