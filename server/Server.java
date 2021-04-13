a6705093c8a4453fbb4df5e6af7998ee8c0ff2c7
Munashe Lawrence Chifungo <munashe.chifungo2@mail.dcu.ie>
2021-04-13 18:17:27 +0100

    To the moooon, whoops wrong project

--- server/Server.java
+++ server/Server.java
@@ -10,13 +10,60 @@ import java.util.*;
 public class Server implements OSserver {
     private final HashMap users = new HashMap<String,String>();
 
+    HashMap<String, HashMap> products;
+
+
+    //Always gets latest prod info
     public List<String[]> getProducts() throws FileNotFoundException { //get items from csv file in Product.java
         Products prod = new Products();
         List<String[]> items = prod.displayProducts(); //returns a list of products & their information
+        System.out.println("displaying items");
+        System.out.println(items);
+        List<String> keys = new ArrayList<String>();
+        keys.add("Quantity");
+        keys.add("Restock Date");
+        keys.add("Restock Quantity");
+
+
+        for (String[] item : items){
+            HashMap<String, Integer> values = new HashMap<String, Integer>();
+            String name = item[0];
+
+            int j = 0;
+            for (int i=1 ; i < item.length; i++){
+
+
+
+                int value = Integer.parseInt(item[i]);
+
+                values.put(keys.get(j), value);
+                j+=1;
+
+
+
+                System.out.println("values: " + values);
+
+
+            }
+
+
+            products.put(name, values);
+
+
+        }
+
+        System.out.println(products);
         return items; //return these products to the client
     }
 
+    public HashMap<String, HashMap> receiveProds() throws FileNotFoundException {
+        getProducts() ;
+        return products;
+    }
+
     public Server() throws RemoteException{
+        this.products = new HashMap<>();
+
         UnicastRemoteObject.exportObject(this,0); //exports the remote object
     }
     public String userlogin(String username, String password){
@@ -40,6 +87,7 @@ public class Server implements OSserver {
             if(username.equals(user)){
                 inMap_check = true;
                 if(password.equals(pass)){
+                    System.out.println("User logged in");
                     response = "Login Successful";
                 }else{
                     response = "Password Incorrect..";
@@ -56,6 +104,49 @@ public class Server implements OSserver {
     private void addUsersToMap(){ //places users into hashmap
         users.put("Admin", "admin");
         users.put("user1", "welcome123");
+    }
+
+    public String confirmOrder(HashMap<String, Integer> order){
+        //Checks if items are in stock
+        Set set = order.entrySet();
+        Iterator iter = set.iterator();
+        String response;
+        response = "";
+
+        while(iter.hasNext()){
+
+
+            Map.Entry entry = (Map.Entry) iter.next();
+
+            String item = entry.getKey().toString();
+//            Integer qty = (Integer) entry.getValue();
+
+            int currentQty = (int) products.get(item).get("Quantity");
+            int orderQty = order.get(item);
+            System.out.println(item + ":" + currentQty + "in stock");
+            System.out.println("The order amount is: " + orderQty);
+
+            if (orderQty > currentQty){
+                response += item + " Out of stock, can't place order\n";
+
+
+            }
+
+            else {
+                response += item + "It's on it's way boss\n";
+            }
+
+
+
+
+
+        }
+
+
+        return response;
+        //If they are, make changes to file
+
+
     }
 
 }
