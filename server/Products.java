8c5ee3224aa910179d2058fabd56a9f935de1074
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-04-14 17:48:09 +0100

    Update Products.java

--- server/Products.java
+++ server/Products.java
@@ -1,14 +1,15 @@
 package server;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class Products {
-
-
 
     HashMap<String, String> products;
     List<String[]> prodList;
@@ -30,8 +31,14 @@ public class Products {
             line = lines.split(" ");
             line_list.add(line);
         }
+        input.close();
         prodList = line_list;
         return line_list; //sends the list of lines to the server
     }
 
+    public void overWrite(StringBuilder map) throws IOException {
+        PrintWriter writer = new PrintWriter("server/Products.csv");
+        writer.println(map);
+        writer.close();
+    }
 }
