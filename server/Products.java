128da26130ca5b239a352ece1200ec81b2d9d6d2
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-04-16 16:06:15 +0100

    ᕕ(⌐■_■)ᕗ ♪♬

--- server/Products.java
+++ server/Products.java
@@ -1,9 +1,6 @@
 package server;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,8 +30,46 @@ public class Products {
         }
         input.close();
         prodList = line_list;
+//        System.out.println("in line listjsshs + " + line_list);
         return line_list; //sends the list of lines to the server
     }
+    public List<String[]> displayOrders(String text) throws java.io.FileNotFoundException {
+        Scanner input = new Scanner(new File("server/Orders.csv"));
+        input.useDelimiter(",|\n");
+        String[] line = new String[0];
+        List<String[]> user_Order_list = new ArrayList<>();
+        while (input.hasNextLine())  //returns a boolean value
+        {
+            String lines = input.nextLine(); //find and returns the next complete token from this scanner
+            if (lines.contains(text)){
+                System.out.println("lines that contain item from Order.csv : " + text + " " + lines);
+                line = lines.split(" ");
+                user_Order_list.add(line);
+        }
+        }
+        input.close();
+        return user_Order_list; //sends the list of lines to the server
+    }
+
+    public void removeOrders(String id) throws IOException {
+        StringBuilder s = new StringBuilder();
+        Scanner input = new Scanner(new File("server/Orders.csv"));
+
+        while (input.hasNextLine())  //returns a boolean value
+        {
+            String lines = input.nextLine(); //find and returns the next complete token from this scanner
+            if (!lines.contains(id)){
+                s.append(lines);
+                s.append("\n");
+            }
+        }
+        input.close();
+
+        PrintWriter writer = new PrintWriter("server/Orders.csv");
+        writer.println(s);
+        writer.close();
+    }
+
 
     public void overWrite(StringBuilder map) throws IOException {
         PrintWriter writer = new PrintWriter("server/Products.csv");
