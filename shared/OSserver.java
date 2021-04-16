2f0fbb5e283a72e41d63d66b7def83e1227849aa
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-04-16 16:02:47 +0100

    REEEE

--- shared/OSserver.java
+++ shared/OSserver.java
@@ -1,8 +1,10 @@
 package shared;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,9 +12,11 @@ import java.util.List;
 //The client communicates with this remote interface.
 public interface OSserver extends Remote {
 
-
     String userlogin(String username, String password) throws RemoteException;
     List<String[]> getProducts() throws RemoteException, FileNotFoundException;
     HashMap<String, HashMap> receiveProds() throws FileNotFoundException, RemoteException;
-    String  confirmOrder(HashMap<String, Integer>  order) throws RemoteException;
+    String  confirmOrder(String date, HashMap<String, Integer> order) throws IOException;
+    int displayPredictAvailability(LocalDateTime date, String text) throws RemoteException, FileNotFoundException;
+    List<String[]>displayPrevOrders(String user) throws RemoteException, FileNotFoundException;
+    void cancelOrder(String id) throws RemoteException, IOException;
 }
